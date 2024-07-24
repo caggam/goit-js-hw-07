f65fb35c9052427fda2f884d6cee481f3bc265b9
caggam <154910250+caggam@users.noreply.github.com>
2024-07-24 16:18:52 +0200

    work1

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input', (event) => {
+  if (event.currentTarget.value.trim() === '') {
+    return nameOutput.textContent = 'Anonymous';
+  }
+
+  nameOutput.textContent = event.currentTarget.value.trim();
+})
---
+++ js/task-6.js
@@ -0,0 +1,35 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const num = document.querySelector('input');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+// --------------------------------------------------------
+createBtn.addEventListener('click', () => {
+  const amount = Number(num.value);
+  num.value = null;
+
+  if(amount < 1 || amount > 100) {
+    return;
+  }
+
+  let div = '';
+  for (let i = 0; i < amount; i++) {
+    const color = getRandomHexColor();
+    const size = 30 + 10 * i + 'px';
+    div += `<div style="width: ${size}; height: ${size}; background-color: ${color}"></div>`;
+  }
+
+  boxes.innerHTML = div;
+})
+
+// --------------------------------------------------------
+destroyBtn.addEventListener('click', () => {
+  num.value = null;
+  boxes.innerHTML = '';
+})
